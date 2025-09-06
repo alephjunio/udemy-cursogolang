c77a25e287d8d27160e83804edebdbe3a5a7bc71
Alef Junio Pereira da Silva <alephjunio@gmail.com>
2025-09-06 14:00:37 -0300

    refactor(desafio_01): rename function and remove debug prints
    Simplify the twoSum function by removing debug prints and renaming it from soma to twoSum for clarity

--- desafios/desafio_01.go
+++ desafios/desafio_01.go
@@ -2,44 +2,25 @@ package main
 
 import "fmt"
 
-// Input: nums = [2, 7, 11, 15], target = 9
-// Output: [0, 1] (porque nums[0] + nums[1] = 2 + 7 = 9)
-
-func soma(nums []int, target int) []int {
-	fmt.Println("\nIniciando função soma com:")
-	fmt.Printf("nums=%v, target=%d\n", nums, target)
-
-	numMap := make(map[int]int)
-	fmt.Println("\nMapa vazio inicializado:", numMap)
-
+func twoSum(nums []int, target int) []int {
+	numIndexMap := make(map[int]int)
 	for i, num := range nums {
-		fmt.Printf("\nIteração %d: Processando num=%d\n", i+1, num)
-
-		complement := target - num
-		fmt.Printf("Complemento calculado: %d - %d = %d\n", target, num, complement)
-
-		if index, exists := numMap[complement]; exists {
-			fmt.Printf("Encontrado complemento %d no índice %d\n", complement, index)
-			fmt.Printf("Retornando par: [%d, %d]\n", index, i)
-			return []int{index, i}
-		}
-
-		numMap[num] = i
-		fmt.Printf("Adicionado ao mapa: numMap[%d] = %d\n", num, i)
-		fmt.Println("Estado atual do mapa:", numMap)
+		numIndexMap[num] = i
 	}
 
-	fmt.Println("\nNenhuma solução encontrada, retornando slice vazio")
+	for i, num := range nums {
+		complement := target - num
+
+		if index, found := numIndexMap[complement]; found && index != i {
+			return []int{i, index}
+		}
+	}
 	return []int{}
 }
 
 func main() {
 	nums := []int{2, 7, 11, 15}
 	target := 9
-
-	fmt.Println("Iniciando programa com:")
-	fmt.Printf("nums=%v\ntarget=%d\n", nums, target)
-
-	result := soma(nums, target)
-	fmt.Printf("\nResultado final: nums=%v, target=%d -> %v\n", nums, target, result)
+	result := twoSum(nums, target)
+	fmt.Println(result)
 }
