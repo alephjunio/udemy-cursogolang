dcd200282133e3f491f17a7ce801671f1b0b4794
Alef Junio Pereira da Silva <alephjunio@gmail.com>
2025-09-08 09:30:18 -0300

    docs(main): add comments explaining Go interfaces
    Add detailed comments explaining Go interfaces, their purpose, and benefits

--- pacote/reta/main.go
+++ pacote/reta/main.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+// Interfaces em Go são uma forma de definir comportamentos
+// Uma interface é um conjunto de métodos que um tipo deve implementar
+//
+// Exemplo de interface:
+// type Forma interface {
+//     Area() float64
+//    Perimetro() float64
+// }
+//
+// Para implementar uma interface, um tipo precisa implementar todos os métodos
+// definidos nela. Em Go, isso é feito implicitamente - não é necessário
+// declarar explicitamente que um tipo implementa uma interface
+//
+// Interfaces permitem:
+// - Polimorfismo
+// - Desacoplamento entre componentes
+// - Testes mais fáceis através de mocks
+// - Maior flexibilidade no código
+
 func main() {
 	p1 := Ponto{2.0, 2.0}
 	p2 := Ponto{2.0, 4.0}
